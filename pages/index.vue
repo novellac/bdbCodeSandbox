c003b24124fc9144896a9bc1b6f2b06e2ef0c021
novellac <38117965+novellac@users.noreply.github.com>
2021-07-26 22:32:41 -0400

    initial commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,25 @@
 <template>
-  <div class="p-4">
-    <div class="max-w-sm rounded overflow-hidden shadow-lg mb-4">
-      <img
-        class="w-full"
-        src="https://tailwindcss.com/img/card-top.jpg"
-        alt="Sunset in the mountains"
-      >
-      <div class="px-6 py-4 pb-2">
-        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-        <p
-          class="text-gray-700 text-base"
-        >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+  <div class="bg-cot-yellow min-h-screen">
+    <div class="max-w-xl mx-auto flex flex-col">
+      <Nav />
+
+      <div class="border flex justify-between">
+        <div class="border">Image Left</div>
+        <div>Date</div>
+        <div class="border">Image right</div>
       </div>
-      <div class="px-6 py-4">
-        <span class="badge mr-2">#photography</span>
-        <span class="badge mr-2">#travel</span>
-        <span class="badge">#winter</span>
+
+      <h1>Happy Birthday YourName</h1>
+      <button class="bg-cot-purple mx-auto">Button to see messages</button>
+
+      <div class="flex justify-between">
+        <div>User image left</div>
+        <div>Static image centre</div>
+        <div>User image right</div>
       </div>
+
+      <div>List of messages when button pressed</div>
+      <div>Footer with credits</div>
     </div>
-    <NuxtLink to="about">
-      <TButton>About</TButton>
-    </NuxtLink>
   </div>
 </template>
-
-<style lang="postcss" scoped>
-.badge {
-  @apply inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700;
-  &:hover {
-    @apply bg-gray-300;
-  }
-}
-</style>
